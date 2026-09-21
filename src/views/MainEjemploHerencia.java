fd1d5d7ed3fd4b9077d6f92f840fefe932ec3777
nicomasmela13-lgtm <nicomasmela13@gmail.com>
2026-09-21 18:21:48 -0500

    Tarea Herencia
    En el main datos quemados para generar 2 alquileres 1 de bicileta electrica y el otro montaña.
    
    Crear una interfaz gráfica, que permita alquilar los 2 tipos de bicicletas que tengo, debo mostrar el costo y el nombre del cliente.

--- src/views/MainEjemploHerencia.java
+++ src/views/MainEjemploHerencia.java
@@ -24,6 +24,27 @@ public class MainEjemploHerencia {
         Alquiler [] arregloAlquileres=new Alquiler[2];
         BicicletaElectrica objElectrica;
         BicicletaMontana objMontana;
+        
+        
+        objElectrica = new BicicletaElectrica(100, 50.0, "E1", "Electrica", true, 5000.0);
+        objMontana = new BicicletaMontana(21, 29.0, "M1", "Montana", true, 4000.0);
+        
+        
+        Alquiler alq1 = new Alquiler(1, 2, "Carlos", objElectrica);
+        alq1.registrarAlquiler();
+        arregloAlquileres[0] = alq1;
+        
+       
+        Alquiler alq2 = new Alquiler(2, 4, "Maria", objMontana);
+        alq2.registrarAlquiler();
+        arregloAlquileres[1] = alq2;
+        
+       
+        System.out.println("Cliente: " + arregloAlquileres[0].getCliente());
+        System.out.println("Costo Total: $" + arregloAlquileres[0].calcularCosto());
+        
+        System.out.println("Cliente: " + arregloAlquileres[1].getCliente());
+        System.out.println("Costo Total: $" + arregloAlquileres[1].calcularCosto());
                 
                 
         
